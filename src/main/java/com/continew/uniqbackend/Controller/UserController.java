fcb04a10727cd4935474dadbb59399f174c89981
Cholong Park <80766477+cholong2@users.noreply.github.com>
2022-10-29 20:04:13 +0900

    로그인 수정

--- src/main/java/com/continew/uniqbackend/Controller/UserController.java
+++ src/main/java/com/continew/uniqbackend/Controller/UserController.java
@@ -1,33 +1,40 @@
 package com.continew.uniqbackend.Controller;
 
+import com.continew.uniqbackend.Service.UserService;
 import com.continew.uniqbackend.UniqBackendApplication;
+import com.continew.uniqbackend.entity.OauthToken;
 import com.continew.uniqbackend.entity.User;
 import com.continew.uniqbackend.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @ResponseBody
-
+@RequestMapping("/api")
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UniqBackendApplication.class);
 
     @Autowired
-    UserRepository userRepository;
+    private UserService userService; //(2)
 
-    @GetMapping("/api/kakao")
-    public String kakaoLogin(String code) {
-        // authorizedCode: 카카오 서버로부터 받은 인가 코드
-        userService.kakaoLogin(code);
+    // 프론트에서 인가코드 받아오는 url
+    @GetMapping("/kakao") // (3)
+    @CrossOrigin(origins = "http://localhost:3000")
+    public OauthToken getLogin(@RequestParam("code") String code) { //(4)
+        log.info(code);
+        // 넘어온 인가 코드를 통해 access_token 발급 //(5)
+        OauthToken oauthToken = userService.getAccessToken(code);
 
-        return "redirect:/";
+//        //(1)
+//        // 발급 받은 accessToken 으로 카카오 회원 정보 DB 저장
+//        String User = userService.saveUser(oauthToken.getAccess_token());
+
+        return oauthToken;
+
     }
 }
--- src/main/java/com/continew/uniqbackend/entity/User.java
+++ src/main/java/com/continew/uniqbackend/entity/User.java
@@ -1,34 +1,48 @@
 package com.continew.uniqbackend.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
+
 @Entity
+@Data
 @NoArgsConstructor
-@Table(name = "user")
+@Table(name = "user") //(1)
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //(2)
+    @Column(name = "user_code") //(3)
+    private Long userCode;
 
-    @Column(name = "name", length = 32)
-    private String name;
+    @Column(name = "kakao_id")
+    private Long kakaoId;
 
-    @Column(name = "email", length = 45)
-    private String email;
+    @Column(name = "kakao_profile_img")
+    private String kakaoProfileImg;
 
-    public Integer getId() {
-        return id;
+    @Column(name = "kakao_nickname")
+    private String kakaoNickname;
+
+    @Column(name = "kakao_email")
+    private String kakaoEmail;
+
+    @Column(name = "user_role")
+    private String userRole;
+
+//    @Column(name = "create_time")
+//    @CreationTimestamp //(4)
+//    private Timestamp createTime;
+
+    @Builder
+    public User(Long kakaoId, String kakaoProfileImg, String kakaoNickname,
+                String kakaoEmail, String userRole) {
+
+        this.kakaoId = kakaoId;
+        this.kakaoProfileImg = kakaoProfileImg;
+        this.kakaoNickname = kakaoNickname;
+        this.kakaoEmail = kakaoEmail;
+        this.userRole = userRole;
     }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
 }
